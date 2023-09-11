60eb79ae863b9ee330ba8e133ec6b19ba7a2ba4f
Qwyatt <65993060+Qwyatt@users.noreply.github.com>
2023-09-11 09:29:27 -0400

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import openai
 from flask import Flask, render_template, request
 import tensorflow as tf
-openai.api_key = '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'
 #load trained NLP model using TensorFlow here
 
 app = Flask(__name__)
